8932d8586c9f0119d2e97096644f6cb5c1c8b1c5
atuanphan <phtuan14@gmail.com>
2026-09-14 19:27:33 +0700

    chore: thêm annotation cho field trong bảng visit_counter

--- be/src/main/java/com/myservice/entity/VisitCounter.java
+++ be/src/main/java/com/myservice/entity/VisitCounter.java
@@ -1,6 +1,9 @@
 package com.myservice.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -9,8 +12,10 @@ import jakarta.persistence.Table;
 public class VisitCounter {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "count")
     private Long count;
 
     public VisitCounter() {
